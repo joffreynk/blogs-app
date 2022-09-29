7a280337f7e9f55f726a76a464ce54139ebf83ff
joffreynk <joyjoffrey@gmail.com>
2022-09-29 23:06:16 +0200

    fix linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     id = params['id'].to_i
-    @user_found = User.find_by(id: id)
+    @user_found = User.find_by(id:)
     @user_found_posts = Post.where(author_id: id)
     @all_comments = Comment.all
   end
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
     id = params['id'].to_i
     user_id = params['user_id'].to_i
     @user_found = User.find_by(id: user_id)
-    @post = Post.find_by(id: id)
+    @post = Post.find_by(id:)
     @all_comments = Comment.where(post_id: id)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,12 +7,10 @@ class Post < ApplicationRecord
   validates_numericality_of :likes_count, greater_than_or_equal_to: 0
   validates_numericality_of :comments_count, greater_than_or_equal_to: 0
 
-
   after_initialize do |post|
     post.likes_count = 0
     post.comments_count = 0
   end
-
 
   def five_most_recent_comments
     comments.order(updated_at: :desc).limit(5)
