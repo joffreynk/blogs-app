deff5b8d67d7855f38672ce57eb9c8e04e906f2e
joffreynk <joyjoffrey@gmail.com>
2022-09-29 23:04:49 +0200

    finalize view part

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,16 @@
 class PostsController < ApplicationController
   def index
-    puts params
+    id = params['id'].to_i
+    @user_found = User.find_by(id: id)
+    @user_found_posts = Post.where(author_id: id)
+    @all_comments = Comment.all
   end
 
   def show
-    puts params
+    id = params['id'].to_i
+    user_id = params['user_id'].to_i
+    @user_found = User.find_by(id: user_id)
+    @post = Post.find_by(id: id)
+    @all_comments = Comment.where(post_id: id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,6 @@ class UsersController < ApplicationController
   def show
     id = params['id'].to_i
     @user_to_find = User.find_by(id: id)
-    @user_found_posts = Post.where(id: id)
+    @user_found_posts = Post.where(author_id: id)
   end
 end
