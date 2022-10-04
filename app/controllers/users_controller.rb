aa186a613374a23c5a4555ffd82fc16ca1589b01
Desor Guishny <desorguishny@gmail.com>
2022-10-04 18:07:27 -0400

    Implement test for user index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
+
   def index
     @users = User.all
   end
+  
+  # User.includes(:most_recent_comments).first.most_recent_comments # => returns all associated comments.
 
   def show
     param_id = params['id'].to_i
 
-    @user_posts = User.where(id:param_id).includes(:posts).where(:posts.limit(3))
+     @user_posts = User.where(id:param_id).includes(:posts)
+    # @user_posts = User.where(id:param_id).includes(:most_recent_posts).first.most_recent_posts
 
 
     @user_to_find = User.find_by(id: param_id)
