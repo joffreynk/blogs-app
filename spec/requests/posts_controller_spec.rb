032536ba672f89c62bb393b09ddfc9b2d4cafc68
JoffreyNK <joyjoffrey@gmail.com>
2022-10-05 09:32:34 +0200

    finalize testing

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,11 +1,60 @@
+# RSpec.describe 'Posts', type: :request do
+#   describe 'GET /posts' do
+#     before do
+#       user = User.create(name: 'Kenobi')
+#       post = Post.create(title: 'The Revenge of the Sith', text: 'Text for testing', author_id: user.id)
+#       get "/users/#{user.id}/posts/#{post.id}/"
+#     end
+#     it 'response status is a success' do
+#       expect(response).to have_http_status(:ok)
+#     end
+#     it "renders 'index' template" do
+#       expect(response).to render_template('show')
+#     end
+#   end
+# end
+
+# RSpec.describe 'sigle post', type: :request do
+#   describe 'GET /posts/:id' do
+#     before do
+#       user = User.create(name: 'Kenobi')
+#       Post.create(title: 'The Revenge of the Sith', text: 'Text for testing', author_id: user.id)
+#       get "/users/#{user.id}/posts/#{post.id}"
+#     end
+#     it 'rsponse status is a success' do
+#       expect(response).to have_http_status(:ok)
+#     end
+#     it "renders 'index' template" do
+#       expect(response).to render_template('show')
+#     end
+#   end
+# end
+
+
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /posts' do
+  describe 'GET /posts/:id' do
     before do
       user = User.create(name: 'Kenobi')
-      post = Post.create(title: 'The Revenge of the Sith', text: 'Text for testing', author_id: user.id)
-      get "/users/#{user.id}/posts/#{post.id}/"
+      Post.create(title: 'The Revenge of the Sith', text: 'Text for testing', author_id: user.id)
+      get "/users/#{user.id}/posts/"
     end
     it 'response status is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
+    end
+  end
+end
+
+RSpec.describe 'sigle post', type: :request do
+  describe 'GET /posts/:id' do
+    before do
+      userx = User.create(name: 'Kenobi')
+      postx = Post.create(title: 'The Revenge of the Sith', text: 'Text for testing', author_id: userx.id)
+      get "/users/#{userx.id}/posts/#{postx.id}/"
+    end
+    it 'rsponse status is a success' do
       expect(response).to have_http_status(:ok)
     end
     it "renders 'index' template" do
@@ -14,18 +63,26 @@ RSpec.describe 'Posts', type: :request do
   end
 end
 
-RSpec.describe 'sigle post', type: :request do
+RSpec.describe 'posts and post page contents', type: :feature do
   describe 'GET /posts/:id' do
     before do
-      user = User.create(name: 'Kenobi')
-      Post.create(title: 'The Revenge of the Sith', text: 'Text for testing', author_id: user.id)
-      get "/users/#{user.id}/posts/#{post.id}"
+      userx = User.create(name: 'Kenobi')
+      postx = Post.create(title: 'The Revenge of the Sith', text: 'Text for testing', author_id: userx.id)
+      visit "/users/#{userx.id}/posts/"
     end
-    it 'rsponse status is a success' do
-      expect(response).to have_http_status(:ok)
+    it 'test the title of the post on post index UI' do
+      expect(page).to have_text('The Revenge of the Sith')
     end
-    it "renders 'index' template" do
-      expect(response).to render_template('show')
+  end
+
+  describe 'GET /posts/:id' do
+    before do
+      userx = User.create(name: 'Kenobi')
+      postx = Post.create(title: 'The Revenge of the Sith', text: 'Text for testing', author_id: userx.id)
+      visit "/users/#{userx.id}/posts/#{postx.id}/"
+    end
+    it 'test text descritpion on post' do
+      expect(page).to have_text('Text for testing')
     end
   end
 end
