9e82d6a3bd6140beec1840ea18874ef2f959d568
joffreynk <joyjoffrey@gmail.com>
2022-10-06 22:20:45 +0200

    working on email confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: %i[index create]
+  before_filter :authenticate_user!, only: %i[index create]
   def index
     @users = User.all
   end
